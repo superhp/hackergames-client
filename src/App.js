d039c37561ff12a22cb905f7b46fdc75f48444fb
Jurgis <jurgis.kargaudas@gmail.com>
2018-03-02 22:18:15 +0200

    Added react and socket.io integration

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
+import { socketConnect } from 'socket.io-react';
 
-const App = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-    <AppBar title="Learn from me" showMenuIconButton={ false } iconElementRight={ <FlatButton label="Home" /> } />
-  </MuiThemeProvider>
-);
+class App extends React.Component {
+
+  sendMessage = () => {
+    this.props.socket.emit('message', 'Hello world from hackergames');
+  }
+
+  render = () => {
+    return <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+      <AppBar title="Learn from me" showMenuIconButton={ false } iconElementRight={ <FlatButton label="Home" /> } />
+      <button onClick={this.sendMessage}>
+         Send msg to socket.io
+         </button>
+    </MuiThemeProvider>
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { SocketProvider } from 'socket.io-react';
+import io from 'socket.io-client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var serverUrl = "http://localhost:1337"
+const socket = io.connect(serverUrl);
+socket.on('message', msg => console.log(msg));
+ 
+const DOMNode = document.getElementById('renderTarget');
+
+ReactDOM.render(
+    <SocketProvider socket={socket}>
+        <App socket={socket}/>
+    </SocketProvider>,
+    document.getElementById('root'));
 registerServiceWorker();
