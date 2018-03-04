77447e07e2cafc93a45d909a3609109e02a9f697
Jurgis <jurgis.kargaudas@gmail.com>
2018-03-04 15:17:34 +0200

    Fixed socket to service.socket

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -47,7 +47,7 @@ class MainPage extends React.Component {
     }
 
     handleMessageReject(e, requesterSocketId) {
-        this.props.socket.emit('reject message', this.props.socket.id, requesterSocketId);
+        this.props.service.getSocket().emit('reject message', this.props.socket.id, requesterSocketId);
     }
 
     filterAndSortUsers(tags) {
